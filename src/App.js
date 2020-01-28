74ddd172c770ea20e9152fcf12b0533b467aa704
Danko <lesko.daniell@gmail.com>
2020-01-28 21:00:17 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import FrameworkList from "./components/FrameworkList";
 import FrameworkPage from "./components/FrameworkPage";
 import HomePage from "./components/HomePage";
 import Layout from "./components/Layout";

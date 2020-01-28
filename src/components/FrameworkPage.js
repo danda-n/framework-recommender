56e626e2c157906631a09737a2d36608edac1f21
Danko <lesko.daniell@gmail.com>
2020-01-28 21:47:07 +0100

    commit

--- src/components/FrameworkPage.js
+++ src/components/FrameworkPage.js
@@ -15,9 +15,65 @@ export default class FrameworkPage extends React.Component {
             </div>
           </div>
         </header>
+        <div className="space-large"></div>
         <div className="page-content">
           <div className="page-content-1">
-            <div className="content-framework-desc">
+            <div className="content-framework-desc spacing-1px">
+              React (also known as React.js or ReactJS) is a JavaScript library
+              for building user interfaces. It is maintained by Facebook and a
+              community of individual developers and companies. React can be
+              used as a base in the development of single-page or mobile
+              applications. However React is only concerned with rendering data
+              to the DOM and so creating React applications usually requires the
+              use of additional libraries for state management, routing, and
+              interaction with an API. Redux, React Router and axios are
+              respective examples of such libraries.
+              <div className="content-framework-2 spacing-1px">
+                <h3>Declarative</h3>
+                <p>
+                  React makes it painless to create interactive UIs. Design
+                  simple views for each state in your application, and React
+                  will efficiently update and render just the right components
+                  when your data changes.
+                </p>
+                <p>
+                  Declarative views make your code more predictable and easier
+                  to debug.
+                </p>
+              </div>
+              <div className="content-framework-3 spacing-1px">
+                <h3>Component-Based</h3>
+                <p>
+                  Build encapsulated components that manage their own state,
+                  then compose them to make complex UIs.
+                </p>
+                <p>
+                  Since component logic is written in JavaScript instead of
+                  templates, you can easily pass rich data through your app and
+                  keep state out of the DOM.
+                </p>
+              </div>
+              <div className="content-framework-3 spacing-1px">
+                <h3>Learn Once, Write Everywhere</h3>
+                <p>
+                  We don’t make assumptions about the rest of your technology
+                  stack, so you can develop new features in React without
+                  rewriting existing code.
+                </p>
+                <p>
+                  React can also render on the server using Node and power
+                  mobile apps using{" "}
+                  <a href="https://facebook.github.io/react-native/">
+                    React Native
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="page-content-2">
+            <div className="content-framework-features">
+              <h3>Features</h3>
               React (also known as React.js or ReactJS) is a JavaScript library
               for building user interfaces. It is maintained by Facebook and a
               community of individual developers and companies. React can be
@@ -28,9 +84,6 @@ export default class FrameworkPage extends React.Component {
               interaction with an API. Redux, React Router and axios are
               respective examples of such libraries.
             </div>
-          </div>
-          <div className="page-content-2">
-            <div className="page-features"></div>
           </div>
         </div>
       </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,10 +5,12 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div className="app-container">
-        <Link to="/">Home</Link>
-        {/* <Link to="/flowchart">Flowchart</Link> */}
-        <Link to="/frameworks">Frameworks</Link>
-        {this.props.children}
+        <div className="app-navigation">
+          <Link to="/">Home</Link>
+          {/* <Link to="/flowchart">Flowchart</Link> */}
+          <Link to="/frameworks">Frameworks</Link>
+        </div>
+        <div className="app-content">{this.props.children}</div>
       </div>
     );
   }

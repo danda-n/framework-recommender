0b07f5b17b222e668b5e37bf934d8cca395e37e9
Danko <lesko.daniell@gmail.com>
2020-01-29 19:25:58 +0100

    onclick card

--- src/components/FrameworkCard.js
+++ src/components/FrameworkCard.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 class FrameworkCard extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("click");
+  }
+
   render() {
     return (
-      <div className="card-framework">
+      <div className="card-framework" onClick={this.handleClick}>
         <div className="card-title">{this.props.title}</div>
         <div className="card-content">{this.props.description}</div>
       </div>
